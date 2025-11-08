9ca4fa56094afa9fb420825c312ca4462a8a899b
panashe12 <49037001+panashe12@users.noreply.github.com>
2025-11-08 16:50:01 +0200

    Delete alx-listing-app directory

--- alx-listing-app/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-blue-600 text-white text-4xl font-bold">
-      Tailwind Works! 🚀
-    </div>
-  );
-}
-
-
